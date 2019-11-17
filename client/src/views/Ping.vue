231b9ecd1dee203def35d0c2d18d40226de1fbbd
Brian Cherinka <havok2063@hotmail.com>
2019-11-16 21:23:20 -0500

    connecting vue client to flask server; adding axios dependency

--- client/src/views/Ping.vue
+++ client/src/views/Ping.vue
@@ -5,12 +5,30 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'ping',
   data() {
     return {
       msg: 'Hello!',
     };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:9000/ping';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
   },
 };
 </script>
